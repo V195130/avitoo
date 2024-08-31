529a717590f57bc5fded30b9c577eb25d9146a2a
v195130 <v195130@yandex.ru>
2024-08-31 16:04:32 +0400

    5 день

--- avitoo-app/src/components/Header/Header.jsx
+++ avitoo-app/src/components/Header/Header.jsx
@@ -1,24 +1,26 @@
-import './Header.css'
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 export const Header = () => {
-    return (
-        <header className="header">
-            <div className="container">
-                <div className="header-box">
-                    <a href="#!" className="header-logo">
-                        <img src="/image/logo.svg" alt="logo" />
-                        <span>Abito</span>
-                    </a>
-                    <div className="header-controls">
-                        <button className="btn btn-outline">Вход и регистрация</button>
-                        <button className="btn btn-primary">Подать обьявление</button>
-                    </div>
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-box">
+          <Link to={`/`} className="header-logo">
+            <img src="/image/logo.svg" alt="Логотип" />
+            <span>Abito</span>
+          </Link>
+          
+          <div className="header-controls">
+            <button className="btn btn-outline">Вход и регистрация</button>
+            <button className="btn btn-primary">Подать объявление</button>
+          </div>
 
-                    <div className="header-burger">
-                        <img src="/image/burger.svg" alt="burger" />
-                    </div>
-                </div>
-            </div>
-        </header>
-    )
-}
+          <div className="header-burger">
+            <img src="/image/burger.svg" alt="burger" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
